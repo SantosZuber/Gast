ff2398f7ccd7f852876ce7aa751e66bdef38b767
Santos Zuberbuhler <santoszuberbuhler2003@protonmail.com>
2022-09-23 20:07:26 -0300

    relationate picture with user WIP

--- index.js
+++ index.js
@@ -3,17 +3,9 @@ const express = require("express");
 const { Server: SocketServer } = require("socket.io");
 const path = require("path");
 const multer = require("multer");
-const { ppid } = require("process");
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "Images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
+const { urlencoded } = require("express");
 
-const upload = multer({ storage: storage });
+//Middleware
 const app = express();
 const httpServer = HTTPServer(app);
 const io = new SocketServer(httpServer);
@@ -24,8 +16,58 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-io.on("connection", (socket) => {});
+//Declaring server storages
+const users = [];
+//Configuring multer to save the img files of the users.
+class Uploader {
+  constructor() {
+    const storage = function () {
+      //No anda la promise, tratar de ver la forma de pasar el dato de la foto al io.on() para asociarlo al usuario. Con una promise
+      //seria lo ideal porque puedo esperar con el .then para que no se ejecute antes el socket y luego con el then le paso la ruta de la imaagen
+      return new Promise((res, rej) => {
+        multer.diskStorage({
+          destination: (req, file, cb) => {
+            cb(null, "Images");
+          },
+          filename: (req, file, cb) => {
+            let fileExt = path.extname(file.originalname);
+            if (fileExt == ".jpg" || fileExt == ".png" || fileExt == ".jpeg") {
+              let filename = Date.now() + path.extname(file.originalname);
+              cb(null, filename);
+              let fileURL = path.join("Images", filename);
+              res(fileURL);
+            }
+          },
+        });
+      });
+    };
+    this.upload = multer({ storage: storage });
+  }
+}
+const uploader = new Uploader();
 
+//Socket.io
+io.on("connection", (socket) => {
+  let myUser;
+  //LOGIN
+  //Una vez se hace el submit del login desde el front ->
+  socket.on("user", (username) => {
+    users.push({ username: username, id: socket.id });
+    myUser = users.filter((e) => {
+      if (e.id == socket.id) {
+        if (e.picture == undefined) {
+          uploader.storage().then((data) => {
+            console.log(data);
+            e.picture = "picture";
+          });
+        }
+        return e;
+      }
+    });
+  });
+});
+
+//Routes
 app.get("/", (req, res) => {
   res.render("index");
 });
@@ -35,7 +77,7 @@ app.get("/login", (req, res) => {
 app.get("/chat", (req, res) => {
   res.render("chat");
 });
-app.post("/login", upload.single("image"), (req, res) => {
+app.post("/login", uploader.upload.single("image"), (req, res) => {
   res.redirect("/chat");
 });
 
--- public/app.js
+++ public/app.js
@@ -1,1 +1,7 @@
 const socket = io.connect();
+document.getElementById("user-input").onsubmit = function (e) {
+  e.preventDefault();
+};
+document.getElementById("login-form").onsubmit = function () {
+  socket.emit("user", document.getElementById("user-input").value);
+};
