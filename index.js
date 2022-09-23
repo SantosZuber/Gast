110461c93baf5ba19b55518be16e27cd202cf78e
Santos Zuberbuhler <santoszuberbuhler2003@protonmail.com>
2022-09-23 14:35:47 -0300

    basic base Add

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const { Server: HTTPServer } = require("http");
 const express = require("express");
 const { Server: SocketServer } = require("socket.io");
 const path = require("path");
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "Images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
 
+const upload = multer({ storage: storage });
 const app = express();
 const httpServer = HTTPServer(app);
 const io = new SocketServer(httpServer);
@@ -10,6 +20,10 @@ const io = new SocketServer(httpServer);
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {});
+
+app.post("/login", upload.single("image"), (req, res) => {
+  res.redirect("/");
+});
 
 httpServer.listen(8080, () => {
   console.log("Server open, port: ", 8080);
