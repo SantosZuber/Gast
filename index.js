413077c7d3b66603bc70350b3eccf41845be2b66
Santos Zuberbuhler <santoszuberbuhler2003@protonmail.com>
2022-09-23 15:44:36 -0300

    ejs Add

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const { Server: SocketServer } = require("socket.io");
 const path = require("path");
 const multer = require("multer");
+const { ppid } = require("process");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "Images");
@@ -17,12 +18,25 @@ const app = express();
 const httpServer = HTTPServer(app);
 const io = new SocketServer(httpServer);
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "public"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 io.on("connection", (socket) => {});
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/chat", (req, res) => {
+  res.render("chat");
+});
 app.post("/login", upload.single("image"), (req, res) => {
-  res.redirect("/");
+  res.redirect("/chat");
 });
 
 httpServer.listen(8080, () => {
