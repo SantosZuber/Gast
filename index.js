3bfcd8a348d8a0de12f949997059edbf0529708a
Santos Zuberbuhler <santoszuberbuhler2003@protonmail.com>
2022-09-25 20:22:06 -0300

    profile pictures didnt work, Remove

--- index.js
+++ index.js
@@ -2,84 +2,49 @@ const { Server: HTTPServer } = require("http");
 const express = require("express");
 const { Server: SocketServer } = require("socket.io");
 const path = require("path");
-const multer = require("multer");
-const { urlencoded } = require("express");
 
 //Middleware
 const app = express();
 const httpServer = HTTPServer(app);
 const io = new SocketServer(httpServer);
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "public"));
+//app.set("views", path.join(__dirname, "public"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Declaring server storages
+//Declaring server data
 const users = [];
-//Configuring multer to save the img files of the users.
-class Uploader {
-  constructor() {
-    const storage = function () {
-      //No anda la promise, tratar de ver la forma de pasar el dato de la foto al io.on() para asociarlo al usuario. Con una promise
-      //seria lo ideal porque puedo esperar con el .then para que no se ejecute antes el socket y luego con el then le paso la ruta de la imaagen
-      return new Promise((res, rej) => {
-        multer.diskStorage({
-          destination: (req, file, cb) => {
-            cb(null, "Images");
-          },
-          filename: (req, file, cb) => {
-            let fileExt = path.extname(file.originalname);
-            if (fileExt == ".jpg" || fileExt == ".png" || fileExt == ".jpeg") {
-              let filename = Date.now() + path.extname(file.originalname);
-              cb(null, filename);
-              let fileURL = path.join("Images", filename);
-              res(fileURL);
-            }
-          },
-        });
-      });
-    };
-    this.upload = multer({ storage: storage });
-  }
-}
-const uploader = new Uploader();
 
 //Socket.io
 io.on("connection", (socket) => {
-  let myUser;
+  let myUser = {};
+  myUser.id = socket.id;
   //LOGIN
   //Una vez se hace el submit del login desde el front ->
-  socket.on("user", (username) => {
-    users.push({ username: username, id: socket.id });
-    myUser = users.filter((e) => {
-      if (e.id == socket.id) {
-        if (e.picture == undefined) {
-          uploader.storage().then((data) => {
-            console.log(data);
-            e.picture = "picture";
-          });
-        }
-        return e;
-      }
-    });
+  socket.on("login", (username) => {
+    validateUsername(username);
+    myUser.username = username;
+    users.push(myUser);
   });
 });
 
-//Routes
-app.get("/", (req, res) => {
-  res.render("index");
-});
-app.get("/login", (req, res) => {
-  res.render("login");
-});
-app.get("/chat", (req, res) => {
-  res.render("chat");
-});
-app.post("/login", uploader.upload.single("image"), (req, res) => {
-  res.redirect("/chat");
-});
+function validateUsername(data) {
+  if (data.includes("<", ">", "*", "/")) {
+    data = "Hacker";
+  } else if (data.includes(" ")) {
+    data = "Hacker";
+  } else if (data === null) {
+    data = "Hacker";
+  }
+}
+function validateMsg(data) {
+  if (data.includes("<", ">", "*", "/")) {
+    data = "There was an error on this message";
+  } else if (data === null) {
+    data = "There was an error on this message";
+  }
+}
 
 httpServer.listen(8080, () => {
   console.log("Server open, port: ", 8080);
--- public/app.js
+++ public/app.js
@@ -2,6 +2,7 @@ const socket = io.connect();
 document.getElementById("user-input").onsubmit = function (e) {
   e.preventDefault();
 };
-document.getElementById("login-form").onsubmit = function () {
-  socket.emit("user", document.getElementById("user-input").value);
+document.getElementById("login-form").onsubmit = function (e) {
+  e.preventDefault();
+  socket.emit("login", document.getElementById("user-input").value);
 };
